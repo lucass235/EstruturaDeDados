011e65736a2a8118e7a7f000841420ade8b46d53
Lucas dos Santos Amorim Rego <lucass235@gmail.com>
2021-05-04 15:38:41 -0300

    Implementação de algumas funcionalidades na lista de LSE Circular

--- ListaLSECircular/src/Aplicacao/Main.java
+++ ListaLSECircular/src/Aplicacao/Main.java
@@ -18,10 +18,29 @@ public class Main {
                 case 1:
                     System.out.print("Informe o valor para ser inserir: ");
                     num = in.nextInt();
-                    lseCircular.inserirValor(num);
+                    lseCircular.inserirValorInicio(num);
                     break;
                 case 2:
-                    lseCircular.exibirInfo();
+                    System.out.print("Informe o valor para ser inserir: ");
+                    num = in.nextInt();
+                    lseCircular.inserirValorFinal(num);
+                    break;
+                case 3:
+                    lseCircular.removerInicio();
+                    break;
+                case 4:
+                    lseCircular.removerFinal();
+                    break;
+                case 5:
+                    System.out.print("Informe o valor para ser virificado: ");
+                    num = in.nextInt();
+                    int qtd = lseCircular.qtdValor(num);
+                    System.out.println("=======================================");
+                    System.out.println("Existem " +qtd+ " Valores!");
+                    System.out.println("========================================");
+                    break;
+                case 6:
+                     lseCircular.exibirInfo();
                     break;
                 case 0:
                     System.out.println("=======================================");
@@ -38,7 +57,11 @@ public class Main {
     }
 
     public static void menu() {
-        System.out.print("1- Inserir um novo valor\n2 - Exibir valor"
+        System.out.print("1- Inserir valor no inicio da lista\n2 - Inserir valor"
+                + "no final da lista\n3 - Remover o primeiro valor da lista"
+                + "\n4 - Remover ultimo valor da lista\n5 - Verificar a"
+                + "quantidade de vezes do valor informado na lista\n6 - "
+                + "Exibir valores da lista"
                 + "\n0 - Encerrar\nInforme o opção desejada: ");
     }
 }
--- ListaLSECircular/src/List/LSECircular.java
+++ ListaLSECircular/src/List/LSECircular.java
@@ -1,14 +1,18 @@
 package List;
 
-public class LSECircular<T> {
+public class LSECircular<T extends Comparable<T> > {
 
     private LSENode<T> prim; // atributo que guarda o endereço do prim nó.
     private LSENode<T> ult; // atributo que guarda o endereço do ult nó.
     private int qtd; // atributo que guarda a qtd de nós na lista.
 
-    public void inserirValor(T info) {
+    public boolean isEmpty() {
+        return this.qtd == 0;
+    }
+
+    public void inserirValorFinal(T info) {
         LSENode<T> newNo = new LSENode(info); // criação do novo nó para inserir na lista.
-        if (qtd == 0) {
+        if (isEmpty()) {
             this.prim = newNo;
             this.ult = newNo;
             ult.setProx(this.prim);
@@ -19,9 +23,28 @@ public class LSECircular<T> {
             ult.setProx(prim);
             qtd++;
         }
-        System.out.println("================================================");
-        System.out.println("Valor inserido com sucesso!");
-        System.out.println("================================================");
+        System.out.printf("%S%n%S%n%S%n",
+                "==============================================",
+                "valor inserido no final da lista!",
+                "==============================================");
+    }
+
+    public void inserirValorInicio(T info) {
+        LSENode<T> newNo = new LSENode(info); // criação do novo nó para inserir na lista.
+        if (isEmpty()) {
+            this.prim = newNo;
+            this.ult = newNo;
+            ult.setProx(this.prim);
+            qtd++;
+        } else {
+            newNo.setProx(this.prim);
+            this.prim = newNo;
+            qtd++;
+        }
+        System.out.printf("%S%n%S%n%S%n",
+                "==============================================",
+                "valor inserido no inicio da lista!",
+                "==============================================");
     }
 
     public void exibirInfo() {
@@ -34,11 +57,85 @@ public class LSECircular<T> {
             aux = this.prim;
             System.out.println("================================================");
             for (int i = 0; i < this.qtd; i++) {
-                System.out.print(aux.getInfo() +" ");
+                System.out.print(aux.getInfo() + " ");
                 aux = aux.getProx();
             }
             System.out.println();
             System.out.println("===============================================");
         }
     }
+
+    public void removerInicio() {
+        if (isEmpty()) {
+            System.out.printf("%S%n%S%n%S%n",
+                    "==============================================",
+                    "lista vazia!",
+                    "==============================================");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.prim = null;
+            this.qtd = 0;
+            System.out.printf("%S%n%S%n%S%n",
+                    "==============================================",
+                    "valor removido, lista vazia!",
+                    "==============================================");
+        } else {
+            this.prim = this.prim.getProx();
+            this.qtd--;
+            System.out.printf("%S%n%S%n%S%n",
+                    "==============================================",
+                    "valor removido!",
+                    "==============================================");
+        }
+    }
+
+    public void removerFinal() {
+        if (isEmpty()) {
+            System.out.printf("%S%n%S%n%S%n",
+                    "==============================================",
+                    "lista vazia!",
+                    "==============================================");
+        } else if (this.qtd == 1) {
+            this.prim = null;
+            this.prim = null;
+            this.qtd = 0;
+            System.out.printf("%S%n%S%n%S%n",
+                    "==============================================",
+                    "valor removido, lista vazia!",
+                    "==============================================");
+        } else {
+            LSENode<T> aux = this.prim;
+            LSENode<T> ant = this.prim;
+            for (int i = 0; i < this.qtd; i++) {
+                ant = aux;
+                if (aux != this.ult) {
+                    aux = aux.getProx();
+                }
+            }
+            this.ult = ant;
+            this.ult.setProx(this.prim);
+            this.qtd--;
+            System.out.printf("%S%n%S%n%S%n",
+                    "==============================================",
+                    "valor removido!",
+                    "==============================================");
+        }
+    }
+    
+    public int qtdValor (T val) {
+       int vezes = 0;
+        if (isEmpty()) {
+           return 0;
+        } else {
+            LSENode<T> aux = this.prim;
+            for (int i = 0; i < this.qtd; i++) {
+                if (aux.getInfo().compareTo(val) == 0) {
+                    vezes++;
+                }
+                aux = aux.getProx();
+            }
+        }
+        return vezes;
+    }
+
 }
